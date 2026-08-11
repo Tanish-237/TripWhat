129d61dbaeb47145f6898d97f2698814469bac41
Samudraneel Sarkar <samudraneel05@gmail.com>
2026-08-12 00:57:26 +0530

    rip out the old ts backend, we're all python now

--- backend/src/services/itinerary/index.ts
+++
@@ -1,3 +0,0 @@
-export { ItineraryEditor, itineraryEditor } from './editor.js';
-export { ItineraryBuilderService, itineraryBuilderService } from './builder.js';
-export * from './types.js';
--- backend/tests/setup.ts
+++
@@ -1,3 +0,0 @@
-// Set dummy API keys for unit tests that import modules with side-effects
-process.env.OPENAI_API_KEY = process.env.OPENAI_API_KEY || 'test-key-for-unit-tests';
-process.env.GOOGLE_PLACES_API_KEY = process.env.GOOGLE_PLACES_API_KEY || 'test-key-for-unit-tests';
